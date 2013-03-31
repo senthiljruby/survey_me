2d48a6e6787883e15a3250b543b1de11f353f2c8
Senthil Kumar Bhaskaran <senthil.jruby@gmail.com>
2013-03-31 19:26:57 +0530

    Adding Seeds to the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+survey_admin = User.create(screen_name: 'admin', full_name: 'Administrator', password: 'admin123',
+                    password_confirmation: 'admin123', admin: true)
+
+survey_taker = User.create(screen_name: 'taker', full_name: 'Survey Taker', password: 'taker123',
+                    password_confirmation: 'taker123')
