3a78cb02c4a1e4ad6c8efe3938ea7e3345b791ba
Senthil Kumar Bhaskaran <bhaskarans@hcl.com>
2013-04-03 16:01:18 +0530

    final fix

--- test/integration/admin_test.rb
+++ test/integration/admin_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class LoginTest < ActionDispatch::IntegrationTest
+class AdminTest < ActionDispatch::IntegrationTest
   setup do
     @admin = users(:admin)
     @admin.password = @admin.password_confirmation = 'admin123'
@@ -9,6 +9,129 @@ class LoginTest < ActionDispatch::IntegrationTest
   end
 
   test 'list of surveys' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    assert has_content?(surveys(:survey_one).name)
+    assert has_content?(surveys(:survey_one).question_type)
+    assert has_content?(surveys(:survey_one).user.screen_name)
+    assert has_content?('Take a survey')
+    assert has_content?('Edit')
+    assert has_content?('Delete')
+    assert has_content?('Results')
+  end
+
+  test 'new Essay Survey' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link 'New Survey'
+    select 'Essay', :from => 'question_type'
+    click_button 'Select Survey'
+    fill_in 'survey_name', :with => 'sample_survey'
+    fill_in 'survey_questions_attributes_0_content', :with => 'Sample Essay Question'
+    click_button 'Create Survey'
+    find('#flash_notice:contains("Survey was successfully created.")')
+  end
+
+  test 'new Numeric Survey' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link 'New Survey'
+    select 'Essay', :from => 'question_type'
+    click_button 'Select Survey'
+    fill_in 'survey_name', :with => 'sample_survey'
+    fill_in 'survey_questions_attributes_0_content', :with => 'Sample Numeric Question'
+    click_button 'Create Survey'
+    find('#flash_notice:contains("Survey was successfully created.")')
+  end
+
+  test 'new Date Survey' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link 'New Survey'
+    select 'Essay', :from => 'question_type'
+    click_button 'Select Survey'
+    fill_in 'survey_name', :with => 'sample_survey'
+    fill_in 'survey_questions_attributes_0_content', :with => 'Sample Date Question'
+    click_button 'Create Survey'
+    find('#flash_notice:contains("Survey was successfully created.")')
+  end
+
+  test 'invalid survey for essay question creation' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link 'New Survey'
+    select 'Essay', :from => 'question_type'
+    click_button 'Select Survey'
+    click_button 'Create Survey'
+    find('.alert:contains("Questions content can\'t be blank")')
+    find('.alert:contains("Name can\'t be blank")')
+  end
+
+  test 'results' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "survey_results_#{@survey.id}"
+    assert has_content?(responses(:one).user.screen_name)
+    assert has_content?(responses(:one).question.content)
+    assert has_content?(responses(:one).user_answer)
+  end
+
+  test 'destroy' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "delete_survey_#{@survey.id}"
+    page.driver.browser.switch_to.alert.accept
+  end
+
+  test 'edit' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "edit_survey_#{@survey.id}"
+    fill_in 'survey_name', :with => 'sample_survey_updated'
+    fill_in 'survey_questions_attributes_0_content', :with => 'Sample Date Question updated'
+    click_button 'Update Survey'
+    find('#flash_notice:contains("Survey was successfully updated.")')
+  end
+
+  test 'invalid edit' do
+    login_as_admin
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "edit_survey_#{@survey.id}"
+    fill_in 'survey_name', :with => ''
+    fill_in 'survey_questions_attributes_0_content', :with => ''
+    click_button 'Update Survey'
+    find('.alert:contains("Questions content can\'t be blank")')
+    find('.alert:contains("Name can\'t be blank")')
+  end
+
+  def login_as_admin
     visit root_path
     within('.container') do
        click_link 'Login'
@@ -16,15 +139,5 @@ class LoginTest < ActionDispatch::IntegrationTest
     fill_in 'user_screen_name', :with => @admin.screen_name
     fill_in 'user_password', :with => 'admin123'
     click_button 'Sign in'
-    visit root_path
-    within('.masthead') do
-      click_link 'Survey'
-    end
-    find("td[0]:contains('#{@survey.name}')")
-    find("td[0]:contains('#{@survey.question_type}')")
-    find("td[0]:contains('#{@survey.user.screen_name}')")
-    find("td[0]:contains('Take a survey')")
-    find("td[0]:contains('Edit')")
-    find("td[0]:contains('Delete')")
   end
 end
---
+++ test/integration/user_test.rb
@@ -0,0 +1,82 @@
+require 'test_helper'
+
+class UserTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:user)
+    @user.password = @user.password_confirmation = 'user123'
+    @user.save!
+    @survey = surveys(:survey_one)
+  end
+
+  test 'list of surveys' do
+    login_as_user
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    assert has_content?(surveys(:survey_one).name)
+    assert has_content?(surveys(:survey_one).question_type)
+    assert has_content?(surveys(:survey_one).user.screen_name)
+    assert has_content?('Take a survey')
+    assert has_no_content?('Edit')
+    assert has_no_content?('Delete')
+    assert has_no_content?('Results')
+  end
+
+  test 'take an survey' do
+    login_as_user
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "take_survey_#{@survey.id}"
+    fill_in "user_answers_#{questions(:question_one).id}", :with => "My First Survey Answer"
+    click_button 'Save'
+    find('#flash_notice:contains("Survey was successfully completed.")')
+  end
+
+  test 'validate presence for survey answer' do
+    login_as_user
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "take_survey_#{@survey.id}"
+    click_button 'Save'
+    find('#flash_notice:contains("User answer can\'t be blank")')
+  end
+
+  test 'validate numerical for numeric survey answer' do
+    login_as_user
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "take_survey_#{surveys(:survey_two).id}"
+    fill_in "user_answers_#{questions(:question_two).id}", :with => "Sample"
+    click_button 'Save'
+    find('#flash_notice:contains("User answer is not a number")')
+  end
+
+  test 'validate date format for date survey answer' do
+    login_as_user
+    visit root_path
+    within('.masthead') do
+      click_link 'Survey'
+    end
+    click_link "take_survey_#{surveys(:survey_three).id}"
+    fill_in "user_answers_#{questions(:question_three).id}", :with => "Sample"
+    click_button 'Save'
+    find('#flash_notice:contains("User answer is not a valid date")')
+  end
+
+  def login_as_user
+    visit root_path
+    within('.container') do
+       click_link 'Login'
+    end
+    fill_in 'user_screen_name', :with => @user.screen_name
+    fill_in 'user_password', :with => 'user123'
+    click_button 'Sign in'
+  end
+end
